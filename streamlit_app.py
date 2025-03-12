aa2e5825228531e93d7df6f867e33fc313ce761f
sotarso <sotargultzz@gmail.com>
2025-03-12 14:56:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,15 @@ st.title('🎶 Machine Learning App')
 
 st.info('This is App build a machine learning Model')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data') :
+  st.write ('***Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
+ 
+  st.write ('***x**')
+  x = df.drop('species', axis = 1)
+  x
+
+  st.write ('***y**')
+  y = df.species
+  y
